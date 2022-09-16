2690815337938dce0968eadee4848f3efbfaeb66
SUBHOJYOTI SINGHA <84560030+subhojyoti99@users.noreply.github.com>
2022-09-17 01:16:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -42,12 +42,24 @@ function App() {
 }
 
 
-function SignIn() {
+var provider = new firebase.auth.GoogleAuthProvider();
 
-  const signInWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider);
-  }
+function googleSignin() {
+   firebase.auth()
+   
+   .signInWithPopup(provider).then(function(result) {
+      var token = result.credential.accessToken;
+      var user = result.user;
+		
+      console.log(token)
+      console.log(user)
+   }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+		
+      console.log(error.code)
+      console.log(error.message)
+   });
 
   return (
     <>
